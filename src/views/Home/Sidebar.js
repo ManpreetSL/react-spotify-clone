95e149d6ebe0c3f84b027b29bccc24c876ed5df3
Manpreet Singh Lalria <ManpreetSL@users.noreply.github.com>
2022-09-19 21:37:58 +0100

    Installed Material UI. Started building the sidebar

--- src/views/Home/Sidebar.js
+++ src/views/Home/Sidebar.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import './Sidebar.css';
+import SidebarOption from './SidebarOption';
+import HomeIcon from '@mui/icons-material/Home';
+
 
 function Sidebar() {
   return (
-    <div className='sidebar'>Sidebar</div>
+    <div className='sidebar'>
+      <img src={ require('../../assets/SpotifyLogoBlack.png') } alt='Spotify logo' className='sidebar__logo'/>
+      <SidebarOption title='Home' />
+      <SidebarOption title='Search' />
+      <SidebarOption title='Your Library' />
+    </div>
   );
 }
 
